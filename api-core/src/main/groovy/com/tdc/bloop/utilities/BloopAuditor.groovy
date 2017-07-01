7322b14ad48992721e169897c891b5cb89e473c7
Ian Gabriel C. Sanchez <iangabrielsanchez@hotmail.com>
2017-07-01 16:10:10 +0800

    fixed project structure

--- api-core/src/main/groovy/com/tdc/bloop/utilities/BloopAuditor.groovy
+++ api-core/src/main/groovy/com/tdc/bloop/utilities/BloopAuditor.groovy
@@ -1,13 +1,16 @@
 package com.tdc.bloop.utilities
 
 import com.esotericsoftware.kryo.Kryo
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.Compile
 import com.tdc.bloop.models.StringRequest
 import com.tdc.bloop.models.StringResponse
+import groovy.transform.CompileStatic
 
 /**
  * Contains methods for registering and au
  * Created by ian.sanchez on 5/31/17.
  */
+@CompileStatic
 class BloopAuditor {
 
     private static final Class[] defaultTypes = [
@@ -17,7 +20,7 @@ class BloopAuditor {
 
     static void registerClasses(Kryo kryo, Class[] dataTypes){
         for(Class dataType: dataTypes){
-            kryo.register( dataTypes )
+            kryo.register( dataType )
         }
     }
 
