865f1c241a046d5be1599cd9fbd5e9610db28a65
Cuzzie <tjakoen.s@gmail.com>
2017-08-27 18:56:18 +0800

    /BA-012-Request-handling:
    IPC class added for Named Pipes

---
+++ bloop-listener-service/src/main/groovy/com/tdc/bloop/listener/model/ipc/ApplicationInformarion.groovy
@@ -0,0 +1,13 @@
+package com.tdc.bloop.listener.model.ipc
+
+/**
+ * Created by tjako on 8/27/2017.
+ */
+class ApplicationInformarion {
+    String name
+    String signature
+    String os
+    String version
+    String listenerMinVersion
+    String listenerMaxVersion
+}
